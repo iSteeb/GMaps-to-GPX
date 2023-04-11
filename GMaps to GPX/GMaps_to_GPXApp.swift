fdc3732d82dd9de5c789f9fa07d664fb428cdd2a
Steven Duzevich <steven@duz.ie>
2023-04-11 10:42:14 +0200

    Initial Commit

---
+++ GMaps to GPX/GMaps_to_GPXApp.swift
@@ -0,0 +1,17 @@
+//
+//  GMaps_to_GPXApp.swift
+//  GMaps to GPX
+//
+//  Created by Steven Duzevich on 11/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct GMaps_to_GPXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
